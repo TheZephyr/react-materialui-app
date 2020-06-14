0653eeeb337c4de9d20d0e632d4e117b8c478ff8
TheZephyr <slodownik.maciej@gmail.com>
2020-06-14 02:31:57 +0200

    Add Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container maxWidth="sm">
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              This app now uses Material-UI
+            </Typography>
+          </Box>
+        </Container>
       </header>
     </div>
   );
